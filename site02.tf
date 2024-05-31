aea06c4d4bb7a8476ad987a71f27e6d7cfd7a6c9
Tom <ThomasJasperSr@proton.me>
2024-05-31 15:05:28 -0400

    deleted commented code

--- site02.tf
+++ site02.tf
@@ -17,26 +17,6 @@ resource "google_compute_subnetwork" "asia_subnetwork" {
   ip_cidr_range = "192.168.0.0/24"
   network = google_compute_network.asia_network.id
 }
-/*
-resource "google_compute_vpn_tunnel" "europe_vpn" {
-  name                  = "europe-vpn"
-  peer_ip               = "x.x.x.x"  # Replace with the IP address of the VPN peer in Europe
-  shared_secret         = "your_shared_secret"  # Replace with your shared secret for the VPN connection
-
-  shared_secret_hash    = base64encode("your_shared_secret")  # Optional: Hashed version of the shared secret
-
-  local_traffic_selector = ["0.0.0.0/0"]  # Allow all traffic from your network to go through the VPN tunnel
-  remote_traffic_selector = ["0.0.0.0/0"]  # Allow all traffic from the remote network to go through the VPN tunnel
-
-  target_vpn_gateway    = google_compute_vpn_gateway.europe_vpn_gateway.self_link
-  target_vpn_gateway_interface = 0  # Index of the interface on the target VPN gateway
-
-  local_ip_address      = "x.x.x.x"  # Replace with the local IP address for the VPN tunnel
-  local_network_interface = "interface_name"  # Replace with the name of the local network interface
-
-  remote_ip_ranges      = ["x.x.x.x/32"]  # Replace with the IP range of the remote network in Europe
-  }
-}*/
 
 resource "google_compute_instance" "asia_instance" {
   name = "vm01"
